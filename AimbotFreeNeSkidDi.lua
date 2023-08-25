7f707be68ddb84b14c2c3d237f7880e7c6acfe5f
clgtmm <103909534+clgtmm@users.noreply.github.com>
2023-08-25 23:33:09 +0700

    Update AimbotFreeNeSkidDi.lua

--- AimbotFreeNeSkidDi.lua
+++ AimbotFreeNeSkidDi.lua
@@ -5,7 +5,7 @@
 local SexyAimbot = nil
 local Mouse = game.Players.LocalPlayer:GetMouse()
 SexyAimbot = hookmetamethod(game, "__index", newcclosure(function(self, Index)
-    if self == Mouse and not checkcaller() and GetClosestPlayer() then
+    if self == Mouse and not checkcaller() then
 		local Part = GetClosestPlayer()
         if Index == "Hit" or Index == "hit" then
             return _G.CFrameTar
