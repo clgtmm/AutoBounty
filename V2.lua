7d367fd583761d8f7b0795bc89deaee3637a7889
clgtmm <103909534+clgtmm@users.noreply.github.com>
2023-12-02 11:05:57 +0700

    Update V2.lua

--- V2.lua
+++ V2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/838f50461eba8785a39c23991b997f32.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/65176e371fa4fc8d372a559f3f97db18.lua"))()
