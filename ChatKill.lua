ef34b4ae75dd459572ab62eb7ada47db576c2298
clgtmm <103909534+clgtmm@users.noreply.github.com>
2024-08-13 21:41:45 +0700

    Delete ChatKill.lua

--- ChatKill.lua
+++
@@ -1,3 +0,0 @@
-local RandomMessage = math.random(1, #getgenv().Chatkill)
-local message = getgenv().Chatkill[RandomMessage]
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message,"All")
