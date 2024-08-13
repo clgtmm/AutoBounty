9cc73d254a294874fefccfb951611a391750137c
clgtmm <103909534+clgtmm@users.noreply.github.com>
2024-08-13 21:41:29 +0700

    Delete SetTransparency.lua

--- SetTransparency.lua
+++
@@ -1,7 +0,0 @@
-for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-      pcall(function()
-          if v.Transparency and v.Parent ~= game.Players.LocalPlayer.Character then
-              v.Transparency = 1
-          end
-      end)
-end
